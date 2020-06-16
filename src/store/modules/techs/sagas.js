d1a1e392b0a5d0a4f9a93e53ea4af47a439b92bb
juliohds <tecjuliohenrique@gmail.com>
2020-06-16 14:50:07 -0300

    mock axios

---
+++ src/store/modules/techs/sagas.js
@@ -0,0 +1,16 @@
+import { call, put } from 'redux-saga/effects';
+
+import api from '../../../services/api';
+
+import { getTechsFailure, getTechsSuccess } from './actions';
+
+export function* getTechs() {
+  try {
+    const response = yield call(api.get, 'techs');
+    console.log(response.data)
+    yield put(getTechsSuccess(response.data));
+  } catch (error) {
+    yield put(getTechsFailure());
+  }
+}
+
